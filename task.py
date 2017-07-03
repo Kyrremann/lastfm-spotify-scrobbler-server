74f28d4b8bf4f4f1cab70eb73321d924c2a436c7
Kyrre Havik Eriksen <kyrremann@gmail.com>
2017-07-03 12:36:47 +0200

    Using forked package, that depends on a MongoDB

--- task.py
+++ task.py
@@ -1,20 +1,8 @@
 from spotify_connect_scrobbler import scrobbler
 import sys, os, json
 
-def create_tmp_config_file():
-    data = {}
-    data["lastfm"] = { "session_key" : os.environ['LASTFM_SESSION_KEY'] }
-    data["spotify"] = { "access_token": os.environ['SPOTIFY_ACCESS_TOKEN'],
-                        "token_type": "Bearer",
-                        "refresh_token": os.environ['SPOTIFY_REFRESH_TOKEN'],
-                        "scope": "user-read-recently-played" }
-    with open('scrobbler.config', 'w') as outfile:
-        json.dump(data, outfile, ensure_ascii=False)
-
 def start_scrobbling():
-    sys.argv = ["task.py", "scrobbler.config"]
     scrobbler.main()
 
 if __name__ == "__main__":
-    create_tmp_config_file()
     start_scrobbling()
