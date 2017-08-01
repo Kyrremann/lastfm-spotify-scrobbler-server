b7dfb89c06c5ac55f54ce09d61b137698d7719c7
Karsten Jeschkies <jeskar@web.de>
2017-08-01 23:46:54 +0200

    Draft authentication with Spotify and Last.fm
    * Direct user to Spotify and Last.fm.
    * Capture tokens from redirects.
    
    TODO:
    * Set proper redirect URL.
    * Make credentials serializable.

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, session
+import os
+from spotify_connect_scrobbler.lastfm import LastfmClient
+from spotify_connect_scrobbler.spotify import SpotifyClient
+
 app = Flask(__name__)
+app.secret_key = 'no so secret' # TODO: Pass key with env variable.
+
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+spotify_client = SpotifyClient(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+
+LASTFM_API_KEY = os.environ['LASTFM_API_KEY']
+LASTFM_API_SECRET = os.environ['LASTFM_API_SECRET']
+lastfm_client = LastfmClient(LASTFM_API_KEY, LASTFM_API_SECRET)
 
 import task
 
@@ -7,10 +20,39 @@ import task
 def hello():
     return "Hello World!"
 
+@app.route("/steps/1")
+def enter_email():
+    # Direct user to authentication Spotify URL.
+    # TODO: Set different redirect url.
+    auth_url = spotify_client.request_authorization()
+    return "<a href='{}'>Authenticate Spotify</a>".format(auth_url)
+
+@app.route("/steps/2")
+def capture_spotify_token():
+    # Retrieve Spotify credentials.
+    code = request.args.get('code', '')
+    spotify_credentials = spotify_client.request_access_token(code)
+    session['spotify_credentials'] = spotify_credentials
+
+    # Direct user to authentication Last.fm URL.
+    # TODO: Set different redirect url.
+    auth_url = lastfm_client.request_authorization()
+    return "<a href='{}'>Authenticate Last.fm</a>".format(auth_url)
+
+@app.route("/steps/3")
+def capture_lastfm_token():
+    # Retrieve Last.fm credentials.
+    token = request.args.get('token', '')
+    lastfm_credentials = lastfm_client.request_access_token(token)
+
+    spotify_credentials = session['spotify_credentials']
+    # TODO: Save credentials to MongoDB
+    return "Last.fm credentials: {}\nSpotify credentials: {}".format(spotify_credentials)
+
 @app.route("/scrobble")
 def manual_scrobbling():
     task.start_scrobbling()
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=4000)
