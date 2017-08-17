4d3027c30c4761054e5b387e297eb20545e8e23d
Kyrre Havik Eriksen <kyrre.havik.eriksen@dfind.no>
2017-08-17 16:56:40 +0200

    Working with a list of users, and updating the data

--- task.py
+++ task.py
@@ -1,11 +1,13 @@
 from spotify_connect_scrobbler import scrobbler
+from database import Database
+
 import sys, os, json
 
 def start_scrobbling():
-    spotify_client = scrobbler.spotify.SpotifyClient(
+    spotify_client = scrobbler.SpotifyClient(
         os.environ['SPOTIFY_CLIENT_ID'],
         os.environ['SPOTIFY_CLIENT_SECRET'])
-    lastfm_client = scrobbler.lastfm.LastfmClient(
+    lastfm_client = scrobbler.LastfmClient(
         os.environ['LASTFM_API_KEY'],
         os.environ['LASTFM_API_SECRET'])
 
@@ -13,13 +15,14 @@ def start_scrobbling():
                   os.environ['MONGODB_DATABASE'],
                   os.environ['MONGODB_COLLECTION'])
 
-    document_id = '599205a1734d1d2227f7b033'
-    users = db.find_credentials(document_id)
-    for user in users:
-        credentials = scrobbler.scrobbler.scrobble(document)
+    document_id = os.environ['MONGODB_DOCUMENT']
+    users = db.find_credentials(document_id)['users']
+    for index, user_document in enumerate(users):
+        credentials = scrobbler.scrobble(user_document, spotify_client, lastfm_client)
         user = credentials.todict()
+        users[index] = user
 
-    db.update_credentials(document_id, users)
+    db.update_credentials(document_id, {'users': users})
 
 
 if __name__ == "__main__":
